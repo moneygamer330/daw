2c582b3395f371c8329185009b81eb63f07b59d2
a24luissr <a24luissr@iessanclemente.net>
2024-10-22 13:23:24 +0200

    dwcc and diw updated

--- dwcc/funciones/functions.js
+++ dwcc/funciones/functions.js
@@ -35,7 +35,7 @@ console.log(
 );
 
 const media = (num) => {
-  num.legnth / num;
+  num + num.lenght / num;
 };
 
 console.log(media(1, 2, 3, 4, 5));
@@ -47,9 +47,7 @@ console.log(
 );
 
 const minMax = (array) => {
-  const min = Math.min(...array);
-  const max = Math.max(...array);
-  return { min, max };
+  return { min: Math.min(...array), max: Math.max(...array) };
 };
 
 console.log(minMax([1, 0, 3, 7, 5]));
@@ -60,8 +58,19 @@ console.log(
   "Ejercicio 6: Crea unha función autoinvocada á que se lle pase a lonxitude e ancho dun rectángulo. A función debe mostrar por consola unha mensaxe indicando o valor da área do rectángulo."
 );
 
-const area = function (lonxitude, ancho) {
-  console.log("A area do rectangulo e: ${lonxitude * ancho}");
-};
+const area = (function (lonxitude = 3, ancho = 4) {
+  const area1 = lonxitude * ancho;
+  return area1;
+})();
 
-area(3, 4);
+console.log(`A area do rectangulo e: ${area}`);
+
+console.log(" ");
+
+console.log(
+  "Exercicio 7: Crea unha función á que se lle pase un DNI (ex: 12345678w ou 87654321T) e devolva se é correcto ou non."
+);
+
+const validarDni = function (dni) {
+  Dni = dni.toString().toUppercase();
+};
