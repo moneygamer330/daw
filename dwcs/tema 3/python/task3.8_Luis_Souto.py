756a648cbb593b1a1313f76594032d628cd6454d
Luis Souto Rey <a24luissr@iessanclemente.net>
2024-11-05 17:59:38 +0100

    django project modified

--- dwcs/tema 3/python/task3.8_Luis_Souto.py
+++ dwcs/tema 3/python/task3.8_Luis_Souto.py
@@ -1,4 +1,16 @@
-#FINISH TASK 3.8
-
 class Alien:
+    numberOfAliens = 0
     
+    def __init__(self, name):
+        self.name = name
+        Alien.numberOfAliens += 1
+    
+    def getNumberOfAliens(self):
+        return Alien.numberOfAliens
+        
+
+alien1 = Alien("Zorg")
+alien2 = Alien("Xanthe")
+alien3 = Alien("Gliese")
+
+Alien.getNumberOfAliens()
