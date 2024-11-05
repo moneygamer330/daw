78bce0e4ebc12cdddee6b77bb79b07aafb729b2c
a24luissr <a24luissr@iessanclemente.net>
2024-11-05 14:16:24 +0100

    dwcc updated, finish tasks

---
+++ dwcc/funciones/regex.js
@@ -0,0 +1,59 @@
+console.log(
+  " 2. Escribe unha expresión regular para comprobar que cada un dos seguintes elementos aparece nunha cadea."
+);
+
+console.log(" ");
+
+console.log("a. car e cat");
+
+console.log("b. pop e prop");
+
+console.log("c. ferret, ferry e ferrari");
+
+console.log("d. Calquera palabra rematada en ious");
+
+console.log("e. Un espazo seguido de punto, coma, dous puntos ou punto e coma");
+
+console.log("f. Unha palabra con máis de 6 letras");
+
+console.log("g. Unha palabra sen a letra e (ou E).");
+
+console.log(" ");
+
+console.log(
+  ". Unha dirección de rede MAC está composta por 6 grupos de dous números hexadecimais separados por ':'. Por exemplo '01:32:54:67:89:AB' Escribe unha expresión regular que comprobe se unha dirección MAC é correcta"
+);
+console.log(" ");
+
+console.log(
+  ". Crea unha función que comprobe se un contrasinal é válido, é dicir, cumpre as seguintes condicións:"
+);
+console.log(" ");
+
+console.log("a. Mínimo 8 caracteres");
+
+console.log("b. Sen espazos en branco");
+console.log(" c. Que teña, polo menos, 3 das seguintes tipos de caracteres:");
+console.log("i. maiúsculas");
+console.log("ii. minúscula");
+console.log("iii. números");
+console.log("iv. caracteres especiais: ¡!$?%&#@/()=¿?*[];,:._<>+-");
+
+console.log(" ");
+console.log(
+  "5. Ás veces é útil eliminar as etiquetas HTML dun texto para evitar que se inclúa código mal intencionado nunha páxina web. /n Crea unha función á que se lle pase un texto e devolva o mesmo texto coas etiquetas HTML eliminadas."
+);
+
+console.log(" ");
+console.log(
+  "6. Dado o seguinte array de insultos, fai un script tal que cada vez que apareza un deles nun texto o substitúa pola primeira letra do insulto e un número de asteriscos igual á lonxitude do insulto - 1"
+);
+
+let insultos = [
+  "testán",
+  "langrán",
+  "fervellasverzas",
+  "baldreo",
+  "lacazán",
+  "pillabán",
+];
