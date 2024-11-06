03a0d0f3465d7d7e018e40900689da2939f128bc
a24luissr <a24luissr@iessanclemente.net>
2024-11-06 11:07:31 +0100

    Password generator updated

--- dwcs/tema 3/django/passwordGenerator/generator/views.py
+++ dwcs/tema 3/django/passwordGenerator/generator/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def  home(request):
-    return HttpResponse('Hello!')
+    return render(request, 'generator/home.html')
+
+def password(request):
+    return render(request, 'generator/password.html')
+    
