0c214e78ee2323fa80c9efb0a57c538da7545681
Luis Souto Rey <a24luissr@iessanclemente.net>
2024-11-06 22:40:26 +0100

    passwordGenerator updated

--- dwcs/tema 3/django/passwordGenerator/password_generator/urls.py
+++ dwcs/tema 3/django/passwordGenerator/password_generator/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('', views.home),
+    path('home/', views.home, name='home'),
     path('generatepassword/', views.password, name='password'),
 ]
