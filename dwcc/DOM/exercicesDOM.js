6208950bbd1bff1d314c8cff107e9603dc97fa2c
Luis Souto Rey <a24luissr@iessanclemente.net>
2024-11-11 22:58:14 +0100

    DOM exercices updated

--- dwcc/DOM/exercicesDOM.js
+++ dwcc/DOM/exercicesDOM.js
@@ -6,30 +6,34 @@ console.log(" ");
 
 console.log("o nodo <div>");
 
-console.log(document.body.children[0]);
+console.log(document.body.childNodes[0]);
 
 console.log(" ");
 
 console.log("o nodo <ul>");
 
-console.log();
+console.log(document.body.childNodes[1]);
 
 console.log(" ");
 
 console.log("o segundo <li>");
+
+console.log(document.body.childNodes[1].childNodes[1]);
 
 console.log(" ");
 
 console.log("2. Dado un elemento calquera dunha árbore DOM: ");
 
 console.log(" ");
-console.log("a. ¿É certo que elemento.lastChild.nextSibling é sempre null?");
+console.log("a. ¿É certo que elemento.lastChild.nextSibling é sempre null?"); //false porque lastChild é o último fillo do elemento     
+
+
 
 console.log(" ");
 
 console.log(
   "b. ¿É certo que elemento.children[0].previousSibling é sempre null?"
-);
+); //true porque children[0] é o primeiro fillo do elemento
 
 console.log(" ");
 
