281c99cfb91c9339eeb0c04cdeb3099a6dbf4e49
Luis Souto Rey <a24luissr@iessanclemente.net>
2024-11-14 21:44:33 +0100

    portfolio project updated

--- dwcs/tema 3/django/portfolioProject/blog/models.py
+++ dwcs/tema 3/django/portfolioProject/blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    title = models.CharField(max_length=200, blank=False, null=False),
-    description = models.TextField(max_length=200, blank=False, null=False, default="Portfolio blog"),
+    title = models.CharField(max_length=200, blank=False, null=False, default="Blog")
+    description = models.TextField(max_length=200, blank=False, null=False, default="Portfolio blog")
     date = models.DateTimeField(auto_now_add=True)
     
--- dwcs/tema 3/django/portfolioProject/blog/views.py
+++ dwcs/tema 3/django/portfolioProject/blog/views.py
@@ -4,5 +4,5 @@ from .models import Blog
 # Create your views here.
 
 def all_blogs(request):
-    blog = Blog.objects.all() #get all posts from the database
-    return render(request, 'blog/all_blogs.html', {'blog':blog})
+    blogs = Blog.objects.all() #get all posts from the database
+    return render(request, 'blog/all_blogs.html', {'blogs':blogs})
