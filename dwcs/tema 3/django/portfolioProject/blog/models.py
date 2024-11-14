c0f708391109562df4e41081eb3d47e584598752
a24luissr <a24luissr@iessanclemente.net>
2024-11-14 14:24:18 +0100

    finish portfolio blog (not worksgit add .)

--- dwcs/tema 3/django/portfolioProject/blog/models.py
+++ dwcs/tema 3/django/portfolioProject/blog/models.py
@@ -3,13 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200)
-    summary = models.CharField(max_length=200)
-    date = models.DateField()
-    url = models.URLField(blank=True)
-
-
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length=200, blank=False, null=False),
+    description = models.TextField(max_length=200, blank=False, null=False, default="Portfolio blog"),
+    date = models.DateTimeField(auto_now_add=True)
     
