5be7b8013dd52b95b8e408ce38bf2c29a2d9975c
a24luissr <a24luissr@iessanclemente.net>
2024-11-15 12:52:35 +0100

    portfolio updated

--- dwcs/tema 3/django/portfolioProject/blog/views.py
+++ dwcs/tema 3/django/portfolioProject/blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
 
 def all_blogs(request):
-    blogs = Blog.objects.all() #get all posts from the database
+    blogs = Blog.objects.order_by('date')[:3] #get all posts from the database
     return render(request, 'blog/all_blogs.html', {'blogs':blogs})
+
+def details(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/details.html', {'blog':blog})
