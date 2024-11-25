7fe0140450a403d5211372b320e7412d71cc2b1e
a24luissr <a24luissr@iessanclemente.net>
2024-11-25 17:38:53 +0100

    js exercices updated

---
+++ dwcc/events/02-eventExercices.js
@@ -0,0 +1,7 @@
+console.log(
+  "1. Engade o JavaScript necesario para que ao pulsar no botón [X] se elimine a mensaxe correspondente."
+);
+
+const x = document.querySelector(".remove-button");
+
+x.addEventListener("click", () => {});
--- dwcs/tema 3/django/MediaCollection/gallery/views.py
+++ dwcs/tema 3/django/MediaCollection/gallery/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from images.models import Image
 from video.models import Video
 
