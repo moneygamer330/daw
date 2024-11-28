49c8ec0fc04a04cfb61abb7bfb7bda6254c3e5c0
a24luissr <a24luissr@iessanclemente.net>
2024-11-28 11:06:17 +0100

    forms exercices updated, not finished

--- dwcc/forms/forms.js
+++ dwcc/forms/forms.js
@@ -1,1 +1,36 @@
-console.log("Crea unha páxina que teña un formulario con unha caixa de texto. Utilizando JavaScript, fai que cando a caixa de texto teña o foco, o borde da mesma sexa verde e cando perda o foco volva ao estilo por defecto. NOTA: non usar CSS border.")
+console.log(
+  "1. Crea unha páxina que teña un formulario con unha caixa de texto. Utilizando JavaScript, fai que cando a caixa de texto teña o foco, o borde da mesma sexa verde e cando perda o foco volva ao estilo por defecto. NOTA: non usar CSS border."
+);
+
+const text = document.getElementById("texto");
+
+text.addEventListener("focus", () => {
+  text.classList.add("foco");
+});
+
+text.addEventListener("blur", () => {
+  text.classList.remove("foco");
+  console.log("Foco perdido");
+});
+
+console.log(" ");
+
+console.log(
+  "2.Crea unha páxina web que conteña un formulario con varios campos: input, select, radiobutton, checkbox, área de texto. Escribe o código JavaScript necesario para mostrar por consola todos os valores dos campos do formulario cando este se envíe."
+);
+
+//TODO finish exercicies
+
+const form2 = document.getElementById("fomr2");
+
+form2.addEventListener("submit", () => {
+  const text1 = document.getElementById("text").value;
+  const select = document.getElementById("select").value;
+  const checkbox = document.getElementById("checkbox").checked;
+  const textarea = document.getElementById("textarea").value;
+
+  console.log("Texto", text1);
+  console.log("Select", select);
+  console.log("Checkbox", checkbox);
+  console.log("Texarea", textarea);
+});
