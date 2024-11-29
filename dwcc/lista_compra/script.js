c5851bb9dd5ad7022b70624209920ce941c188aa
a24luissr <a24luissr@iessanclemente.net>
2024-11-29 10:22:05 +0100

    js buy list updated, not finished

---
+++ dwcc/lista_compra/script.js
@@ -0,0 +1,15 @@
+const div = document.getElementById("div");
+const addElement = document.getElementById("addElement");
+const filterElements = document.getElementById("filterElements");
+const deleteElements = document.getElementById("deleteElements");
+const input = document.getElementById("element");
+const filter = document.getElementById("filter");
+const list = document.getElementById("list");
+let li = document.createElement("li");
+
+//add element to the list
+addElement.addEventListener("click", () => {
+  li.append(input.value.trim());
+  list.append(li);
+  list.append(div);
+});
